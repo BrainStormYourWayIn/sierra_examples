b9780e8c2f89acf4fe59b063bba954f1499c693c
pranavr2003 <56883253+pranavr2003@users.noreply.github.com>
2021-06-14 19:56:19 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -256,6 +256,7 @@ if __name__ == "__main__":
 
     openTags('pre')
     text = f'''
+    
     a = addImg("location/to/the/image.jpeg", href="https://google.com", alt="Some image", img_class="newImg")
 
     a.show()                    # Takes no argument and displays the image with the conditions given
